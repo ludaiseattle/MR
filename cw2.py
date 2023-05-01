3a6ec78c806799e3d5c13686d84ba419d487658e
eric <dailucool@gmail.com>
2023-05-02 02:38:56 +0800

    could send email

--- cw2.py
+++ cw2.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
+import smtplib
 
 app = Flask("Novelist")
 
 history = []
+emails = []
+
+def send_email(subject, message, from_email, to_email):
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.starttls()
+    server.login(from_email, 'MR@666MR@666') 
+    msg = f'Subject: {subject}\n\n{message}'
+    server.sendmail(from_email, to_email, msg.encode('utf-8'))
+    server.quit()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,6 +22,17 @@ def index():
         input_text = request.form['input_text']
         history.append(input_text)
         output_text = '<br>'.join(history) + '<br>'
+
+        email = request.form['email']
+        if email:
+            emails.append(email)
+        if len(emails):
+            from_email = 'mixedreality888@outlook.com' 
+            subject = 'Story Content'
+            message = f'New Content: {input_text}'
+            for to_email in emails:
+                send_email(subject, message, from_email, to_email)
+
     return render_template('index.html', input_text=input_text, output_text=output_text)
 
 if __name__ == '__main__':
